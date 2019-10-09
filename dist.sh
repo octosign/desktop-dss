fa7f2bb29febd36d8a498ad5479a3fc571a08d1f
Jakub Ďuraš <jakub@duras.me>
2019-10-09 21:26:47 +0200

    Update dist config so we don't package ourselves

--- dist.sh
+++ dist.sh
@@ -11,8 +11,3 @@ cp backend.yaml ./dist/
 cp -r ./target/preparedJDK ./dist/jdk
 cp -r ./target/dependency-jars ./dist/
 cp ./target/octosigndss*.jar ./dist/sign.jar
-
-# TODO: Create checksum and sign
-
-# Create archive
-XZ_OPT=-3 tar cJf tarfile.tar.xz dist/*
