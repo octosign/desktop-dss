09e0def872da05ec49395df6733df9daadd752d8
Jakub Ďuraš <jakub@duras.me>
2020-01-05 13:01:50 +0100

    Fix copied backend.yml ext

--- dist.sh
+++ dist.sh
@@ -7,7 +7,7 @@ rm -rf ./dist && mkdir ./dist
 mvn package -f pom.xml
 
 # Copy files to dist directory
-cp backend.yaml ./dist/
+cp backend.yml ./dist/
 cp -r ./target/preparedJDK ./dist/jdk
 cp -r ./target/dependency-jars ./dist/
 cp ./target/octosigndss*.jar ./dist/sign.jar
