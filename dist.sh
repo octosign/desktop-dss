9f9f1ab41ee42fe2e6eccec10f585a91d7f06abd
Jakub Ďuraš <jakub@duras.me>
2020-01-20 01:15:11 +0100

    Add license

--- dist.sh
+++ dist.sh
@@ -8,6 +8,7 @@ mvn package -f pom.xml
 
 # Copy files to dist directory
 cp backend.yml ./dist/
+cp LICENSE ./dist/
 cp -r ./target/preparedJDK ./dist/jdk
 cp -r ./target/dependency-jars ./dist/
 cp ./target/octosigndss*.jar ./dist/sign.jar
