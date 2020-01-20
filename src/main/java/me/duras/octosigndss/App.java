bdc82aa161f8929ef2581b0c819f22491d2f48b7
Jakub Ďuraš <jakub@duras.me>
2020-01-20 01:49:01 +0100

    Add dispatch of operation, pkcs dll finding

--- src/main/java/me/duras/octosigndss/App.java
+++ src/main/java/me/duras/octosigndss/App.java
@@ -2,6 +2,7 @@ package me.duras.octosigndss;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Locale;
 
 import eu.europa.esig.dss.enumerations.DigestAlgorithm;
 import eu.europa.esig.dss.enumerations.SignatureLevel;
@@ -17,19 +18,35 @@ import eu.europa.esig.dss.token.Pkcs11SignatureToken;
 import eu.europa.esig.dss.validation.CommonCertificateVerifier;
 
 /**
- * Hello world!
- *
+ * DSS signing backend app
  */
 public class App {
     public static void main(String[] args) throws IOException {
-        File fileToSign = new File(args[1]);
+        if (args[0].equals("meta")) {
+            System.err.println("Meta operation is not yet supported");
+            System.exit(1);
+        } else if (args[0].equals("sign")) {
+            App.sign(args[1]);
+        } else if (args[0].equals("verify")) {
+            System.err.println("Verify operation is not yet supported");
+            System.exit(1);
+        } else {
+            System.err.println("Unsupported operation " + args[0]);
+            System.exit(1);
+        }
+    }
+
+    private static void sign(String filePath) throws IOException {
+        String pkcsDllPath = App.findPkcsDllPath();
+        if (pkcsDllPath == null) {
+            System.err.println("Can not find supported PKCS DLL.");
+            System.exit(1);
+        }
+
+        File fileToSign = new File(filePath);
         DSSDocument document = new FileDocument(fileToSign);
 
-        // TODO: Try automatically
-        // C:\\Program Files (x86)\\EAC MW klient\\pkcs11_x64.dll
-        // /usr/lib/eac_mw_klient/libpkcs11_x64.so
-        try (Pkcs11SignatureToken token = new Pkcs11SignatureToken(
-                "C:\\Program Files (x86)\\EAC MW klient\\pkcs11_x64.dll", new PasswordCallback(), 1)) {
+        try (Pkcs11SignatureToken token = new Pkcs11SignatureToken(pkcsDllPath, new PasswordCallback(), 1)) {
 
             DSSPrivateKeyEntry privateKey = token.getKeys().get(0);
 
@@ -73,5 +90,37 @@ public class App {
 
             signedDocument.save(fileToSign.getAbsolutePath().replace(".pdf", "-signed.pdf"));
         }
+    }
+
+    private static String findPkcsDllPath() {
+        String[] windowsPkcsDlls = {
+                // Slovak eID default installation directory
+                "C:\\Program Files (x86)\\EAC MW klient\\pkcs11_x64.dll" };
+        String[] linuxPkcsDlls = {
+                // Slovak eID default installation directory
+                "/usr/lib/eac_mw_klient/libpkcs11_x64.so" };
+        String[] darwinPkcsDlls = {
+                // Slovak eID default installation directory
+                "/Applications/Aplikacia_pre_eID.app/Contents/pkcs11/libPkcs11.dylib" };
+
+        String osName = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
+
+        String[] paths;
+        if ((osName.indexOf("mac") >= 0) || (osName.indexOf("darwin") >= 0)) {
+            paths = darwinPkcsDlls;
+        } else if (osName.indexOf("win") >= 0) {
+            paths = windowsPkcsDlls;
+        } else if (osName.indexOf("nux") >= 0) {
+            paths = linuxPkcsDlls;
+        } else {
+            return null;
+        }
+
+        for (String path : paths) {
+            if ((new File(path)).exists())
+                return path;
+        }
+
+        return null;
     }
 }
